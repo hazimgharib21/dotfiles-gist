5da6c99978516234a75e6ec227109b69c53e3760
hazimgharib <hazimgharib@e-shore.com.my>
2020-11-27 14:49:30 +0800

    add formatting dependencies

--- install-dotfile.sh
+++ install-dotfile.sh
@@ -5,15 +5,15 @@ echo " *** Installing git, byobu, vim"
 echo ""
 
 sudo apt purge -y vim vim-tiny
+sudo apt update -y
 sudo apt install -y git byobu vim-nox
 
-echo ""
-echo " *** Install jsbeautify "
-echo ""
-
-chmod +x install-node.sh
-./install-node.sh
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 9FD3B784BC1C6FC31A8A0A1C1655A0AB68576280
+sudo sh -c 'echo "deb https://deb.nodesource.com/node_10.x $(lsb_release -sc) main" > /etc/apt/sources.list.d/nodesource.list'
+sudo apt update -y
+sudo apt install nodejs
 sudo npm install -g js-beautify
+sudo pip install autopep8
 
 echo ""
 echo " *** Install astyle"
